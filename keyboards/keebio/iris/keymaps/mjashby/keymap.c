e735ef187e9750b1521e3c87fc1ec1096ed50dc7
Mike Ashby <mike@myrtle.ai>
2021-01-12 17:23:35 +0000

    cycle enter, escape, backspace. move ctrl, shift, alt, layer accessors

--- keyboards/keebio/iris/keymaps/mjashby/keymap.c
+++ keyboards/keebio/iris/keymaps/mjashby/keymap.c
@@ -21,11 +21,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB,  KC_SLSH, KC_D,    KC_R,    KC_W,    KC_B,                               KC_J,    KC_F,    KC_U,    KC_P,    KC_SCLN, KC_QOUT,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_BSPC, KC_A,    KC_S,    KC_H,    KC_T,    KC_G,                               KC_Y,    KC_N,    KC_E,    KC_O,    KC_I,    KC_ESC,
+     KC_ESC,  KC_A,    KC_S,    KC_H,    KC_T,    KC_G,                               KC_Y,    KC_N,    KC_E,    KC_O,    KC_I,    KC_ENT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,    KC_GRV,           KC_DEL,  KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_Q,    KC_RSFT,
+     KC_LCTL, KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,    KC_GRV,           KC_DEL,  KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_Q,    KC_RALT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LCTL, LOWER,   KC_ENT,                    KC_SPC,  RAISE,   KC_RALT
+                                    MO(_NUM),KC_LSFT, KC_BSPC,                   KC_SPC,  KC_RSFT, TG(_NAV)
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
