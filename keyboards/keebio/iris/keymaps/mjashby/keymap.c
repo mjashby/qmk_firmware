1c9eb99245197ab21acadf9b1cd57e6a3deb8e95
Mike Ashby <mike@myrtle.ai>
2021-01-14 11:42:01 +0000

    make tab more accessible

--- keyboards/keebio/iris/keymaps/mjashby/keymap.c
+++ keyboards/keebio/iris/keymaps/mjashby/keymap.c
@@ -23,9 +23,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB,  KC_SLSH, KC_D,    KC_R,    KC_W,    KC_B,                               KC_J,    KC_F,    KC_U,    KC_P,    KC_AT,   KC_GRV,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_ESC,  KC_A,    KC_S,    KC_H,    KC_T,    KC_G,                               KC_Y,    KC_N,    KC_E,    KC_O,    KC_I,    KC_ENT,
+     KC_TAB,  KC_A,    KC_S,    KC_H,    KC_T,    KC_G,                               KC_Y,    KC_N,    KC_E,    KC_O,    KC_I,    KC_ESC,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,    KC_BSPC,          KC_DEL,  KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_Q,    KC_RALT,
+     KC_LCTL, KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,    KC_BSPC,          KC_DEL,  KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_Q,    KC_LALT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     TG(_NAV),KC_LSFT, KC_ENT,                    KC_SPC,  KC_RSFT, MO(_NUM)
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
